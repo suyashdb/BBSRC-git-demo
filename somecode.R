0657ad30fbcb22b3fa3808e5c0ab720ea37d102d
Suyash <suyashdb@gmail.com>
2017-03-24 12:27:24 -0700

    adding outlier removal

--- somecode.R
+++ somecode.R
@@ -2,6 +2,7 @@
 df=read.table('http://data.bris.ac.uk/datasets/swyt56qr4vaj17op9cw3sag7d/LskyetalPLOSONE.csv',
               header=TRUE,sep=',')
 head(df)
+df=subset(df,age>min_age&age<max_age)
 lm.result=lm(conspiracist_avg~age,data=df)
 summary(lm.result)
 max_age=120
